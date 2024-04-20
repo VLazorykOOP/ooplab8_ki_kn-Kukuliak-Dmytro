61eeeaf123e5b7a4b30a9f695b04c221f25f8e1f
Kukuliak-Dmytro <kukuliak.dmytro.clg@chnu.edu.ua>
2024-04-20 09:56:20 +0300

    V1

--- OOPLab8T/OOPLab8T.cpp
+++ OOPLab8T/OOPLab8T.cpp
@@ -1,14 +1,12 @@
-﻿// OOPLab8T.cpp :  for Lab7 (new)
-//
-
-#include <iostream>
+﻿#include <iostream>
 #include "example.h"
 int main()
 {
-    int r;       
-    std::cout << "Lab 7 (new)\n";
-     std::cout << " Run example press 1 \n";
-     std::cin >> r; if (r == 1) example();
-    
+	SortFind<char> test1(50);
+	//std::cout << test1;
+	test1.randomize();
+	std::cout << test1;
+	std::cout <<test1.find(2);
+
 }
 
--- OOPLab8T/example.h
+++ OOPLab8T/example.h
@@ -1,2 +1,37 @@
 #pragma once
-void example();
+template <typename T>
+class SortFind {
+private:
+    size_t n;
+    T* arr;
+public:
+    SortFind(size_t N){
+        this->n = N;
+        arr = new T[N]; // Allocate memory for the array of pointers
+    }
+
+    ~SortFind() {
+        delete[] arr;
+    }
+    void randomize() {
+        srand(time(NULL));
+        for (size_t i = 0; i < n; i++) {
+            arr[i] = static_cast<T>(rand() % 128 );
+        }
+    }
+    friend std::ostream& operator<<(std::ostream& os, SortFind<T>& a) {
+        for (int i = 0; i < a.n; i++) {
+            os << a.arr[i] << " ";
+        }
+        os << std::endl;
+        return os;
+    }
+    template <typename T> int find(T a) {
+        int index=-1;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == a)index = i;
+        }
+        return index;
+    }
+
+};
